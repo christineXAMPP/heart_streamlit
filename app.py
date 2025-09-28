8f5383b20a88d6525fca680e1baf69f9c311ac0b
christineXAMPP <c.tapat.539161@umindanao.edu.ph>
2025-09-28 18:35:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.markdown(
 
     /* Full-width button */
     div.stButton > button {
-        width: 320%;
+        width: 340%;
         height: 46px;
         font-size: 15px;
         border-radius: 8px;
